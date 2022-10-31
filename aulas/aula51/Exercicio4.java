c419ecbc65aeb22dd4866e00af9aff6c233dbcb8
Guilherme Gomes <guilhermegomes.csilva@gmail.com>
2022-10-31 20:44:11 -0300

    Criando Classe

---
+++ aulas/aula51/Exercicio4.java
@@ -0,0 +1,18 @@
+package aulas.aula51;
+import java.util.Scanner;
+
+public class Exercicio4 {
+       
+    public static void main(String[]args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Informe um valor:");
+        int num = input.nextInt();
+
+        System.out.printf("O valor informado foi: %s", num);
+
+    }
+
+
+}
