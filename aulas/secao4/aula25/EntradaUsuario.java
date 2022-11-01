c7a1660f090418679eb0d5e11eadc82928d30edb
Guilherme Gomes <guilhermegomes.csilva@gmail.com>
2022-10-31 21:39:02 -0300

    Inserindo dica para o comando nextInt ou NextLine...

--- aulas/secao4/aula25/EntradaUsuario.java
+++ aulas/secao4/aula25/EntradaUsuario.java
@@ -8,7 +8,7 @@ public class EntradaUsuario {
     public static void main(String[]args){
     
     Scanner input = new Scanner(System.in);
-    
+    /*
     String s1, s2, s3;
 
     s1 = input.nextLine();
@@ -19,6 +19,28 @@ public class EntradaUsuario {
     System.out.println(s1);
     System.out.println(s2);
     System.out.println(s3);
+        */
+
+    /*ATENÇÃO COM A QUEBRA DE LINHA PENDENTE: 
+    quando usamos o comando next , ao apertar o enter ele faz uma quebra de linha, 
+    sendo que, essa quebra conta no programa!!!!
+        EXEMPLO ABAIXO
+    */
+
+    String s1, s2, s3;
+    int x;
+
+    x = input.nextInt();
+    s1 = input.nextLine();
+    s2 = input.nextLine();
+    s3 = input.nextLine();
+
+    System.out.println("DADOS DIGITADOS:");
+    System.out.println(x);
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(s3);
+
 
     }
 
