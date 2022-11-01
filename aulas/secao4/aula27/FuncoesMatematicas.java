1501056e9e911b1dec66e290d11b81b5b7b888ff
Guilherme Gomes <guilhermegomes.csilva@gmail.com>
2022-11-01 12:38:55 -0300

    Utilizando a função Math.sqrt

---
+++ aulas/secao4/aula27/FuncoesMatematicas.java
@@ -0,0 +1,15 @@
+package aulas.secao4.aula27;
+
+import java.util.Scanner;
+
+public class FuncoesMatematicas {
+    public static void main(String[]args){
+        double a, b = 81.0, c;
+
+        a = Math.sqrt(b);
+
+        System.out.printf("A raiz quadrada de %.2f é: %.2f", b, a);
+
+
+    }
+}
